4fa6ba2f160192ee9bfd4f529a188253cf25ad9b
Mohammad Shahrukh <mohdshahrukh216@gmail.com>
2019-09-12 22:22:54 +0530

    computer1 1st 3 chapter

---
+++ data/MST1.js
@@ -0,0 +1,306 @@
+var data= {
+  "QuestionPaper": {
+    "-UniqueId": "002",
+    "-CategoryName": "Computer - 1",
+    "Chapter": [
+      {
+        "-ChapterName": "The Computer: A Machine",
+        "-Heading": "Multiple Choice Questions",
+        "Cluster": [
+          
+          {
+            "-QType": "FillUp",
+            "-Heading": "Fill in the blanks:",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tA computer is a __________."
+              },
+              {
+                "-Question": "2.\tComputer never gets __________."
+              },
+              {
+                "-Question": "3.\tSome machines run on __________."
+              },
+              {
+                "-Question": "4.\tA computer works very __________."
+              },
+              {
+                "-Question": "5.\tA computer never makes __________."
+              }
+            ]
+          },
+		  {
+            "-QType": "FillUp",
+            "-Heading": "Write ‘T’ for true and ‘F’ for false:",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tA fridge is found outside our houses.\t______"
+              },
+              {
+                "-Question": "2.\tAn iron is not a machine.\t\t\t\t______"
+              },
+              {
+                "-Question": "3.\tA computer is a machine.\t\t\t\t______"
+              },
+              {
+                "-Question": "4.\tMachines help us to save time.\t\t\t______"
+              },
+              {
+                "-Question": "5.\tA computer sometimes get tired.\t\t______"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Tick the correct option.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tMachine are __________ things.\n\n(a) natural      (b) man-made     (c) magical\r\n"
+              },
+              {
+                "-Question": "2.\tA computer runs on __________.\n\t(a) fuel      (b) electricity    (c) human power\n"
+              },
+              {
+                "-Question": "3.\tWhich machine provides us fresh and cool air?\n\t(a) Refrigerator      (b) Air conditioner   (c) Bicycle\n"
+              },
+              {
+                "-Question": "4.\tA __________ is a computer we keep on a table.\n\t(a) Desktop      (b) Laptop   (c) Mobile phone\n"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Answer the following questions.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tName a machine the runs on electricity."
+              },
+              {
+                "-Question": "2.\tName a machine that runs on fuel."
+              },
+              {
+                "-Question": "3.\tWhich is a smart machine?"
+              },
+              {
+                "-Question": "4.\tWrite any one machine you found in your classroom."
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Picture Based Question",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-OptionImage": "MST1/1.jpg"
+              },
+			  {
+                "-OptionImage": "MST1/2.jpg"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "-ChapterName": "Parts of a Computer",
+        "-Heading": "Answer the following.",
+        "Cluster": [
+          {
+            "-QType": "FillUp",
+            "-Heading": "Fill in the blanks:",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tThe CPU is the _______ of the computer."
+              },
+              {
+                "-Question": "2.\tA keyboard has many buttons called ________."
+              },
+              {
+                "-Question": "3.\tA computer has ________ main parts."
+              },
+              {
+                "-Question": "4.\tWe use a ________ to type."
+              },
+              {
+                "-Question": "5.\tA ________ is used to point at items on the monitor."
+              }
+            ]
+          },
+		  {
+            "-QType": "FillUp",
+            "-Heading": "Write ‘T’ for true and ‘F’ for false:",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tA keyboard has one button.\t\t\t\t______"
+              },
+              {
+                "-Question": "2.\tA monitor has a screen.\t\t\t\t\t______"
+              },
+              {
+                "-Question": "3.\tA mouse has two buttons and a wheel.\t\t______"
+              },
+              {
+                "-Question": "4.\tA computer has two keyboards.\t\t\t______"
+              },
+              {
+                "-Question": "5.\tThe printed paper is called printout.\t\t\t______"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Tick the correct option.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tA monitor looks like a:\r\n(a)\tmouse      (b) television     (c) typewriter\r\n"
+              },
+              {
+                "-Question": "2.\tIt is the brain of the computer.\n(a) CPU      (b) UPS    (c) Monitor\n"
+              },
+              {
+                "-Question": "3.\tThis is used to print our work on paper.\n\n(a) Speakers      (b) Mouse   (c) Printer\n"
+              },
+              {
+                "-Question": "4.\tIt keeps the computer ON even if the electricity goes OFF.\n\n(a) CPU      (b) UPS   (c) Mouse\n"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Answer the following questions.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWhich part of a computer helps us to type?"
+              },
+              {
+                "-Question": "2.\tWhich part of a computer looks like a television?"
+              },
+              {
+                "-Question": "3.\tWhat are the buttons on a keyboard called?"
+              },
+              {
+                "-Question": "4.\tWhat does CPU stand for?"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Picture Based Question",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-OptionImage": "MST1/3.jpg"
+              },
+			  {
+                "-OptionImage": "MST1/4.jpg"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "-ChapterName": "Uses of Computers",
+        "-Heading": "Answer the following.",
+        "Cluster": [
+          {
+            "-QType": "FillUp",
+            "-Heading": "Fill in the blanks:",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWe can solve ____ on a computer."
+              }, 
+              {
+                "-Question": "2.\tWe can _____ to music on a computer."
+              },
+              {
+                "-Question": "3.\tWe can play ______ on a computer."
+              },
+              {
+                "-Question": "4.\tWe can watch ______ using a computer."
+              },
+              {
+                "-Question": "5.\tWe can type ______ on a computer."
+              }
+            ]
+          },
+		  {
+            "-QType": "FillUp",
+            "-Heading": "Write ‘T’ for true and ‘F’ for false:",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWe can draw and colour on a computer.\t\t\t______"
+              },
+              {
+                "-Question": "2.\tWe can cook food on a computer.\t\t\t\t______"
+              },
+              {
+                "-Question": "3.\tWe cannot solve sums on a computer.\t\t\t______"
+              },
+              {
+                "-Question": "4.\tYou can make projects on a computer.\t\t\t______"
+              },
+              {
+                "-Question": "5.\tWe cannot book tickets by computer.\t\t\t______"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Tick the correct option.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tComputer are used to:\n\t(a) play games      (b)watch movies     (c) both of these\r\n"
+              },
+              {
+                "-Question": "2.\tA computer is used to listen to:\n\t(a) pictures      (b) music    (c) games\n"
+              },
+              {
+                "-Question": "3.\tTeachers use computers to make:\n\t(a) bills      (b) letters   (c) report cards\n"
+              },
+              {
+                "-Question": "4.\tComputers are used to keep records of :\n\t(a) flights      (b) doctors   (c) letters\n"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Answer the following questions.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWhat can we watch on a computer?"
+              },
+              {
+                "-Question": "2.\tName a place where computers are used to type letters?"
+              },
+              {
+                "-Question": "3.\tWhich machine is used to book tickets?"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Picture Based Question",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-OptionImage": "MST1/5.jpg"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
