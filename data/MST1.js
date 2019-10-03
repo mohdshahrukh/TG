04a72bee350c44b9eabb8de66a568d1179833be4
Mohammad Shahrukh <mohdshahrukh216@gmail.com>
2019-10-03 17:24:55 +0530

    computer all chapter completed

--- data/MST1.js
+++ data/MST1.js
@@ -300,6 +300,413 @@ var data= {
             ]
           }
         ]
+      },
+	  {
+        "-ChapterName": "Know the Keyboard",
+        "-Heading": "Answer the following.",
+        "Cluster": [
+          {
+            "-QType": "FillUp",
+            "-Heading": "Fill in the blanks:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tWe use a _______ to write on a computer."
+              },
+              {
+                "-Question": "2.\tThere are two sets of ________ keys."
+              },
+              {
+                "-Question": "3.\t ________ is the longest key on the keyboard."
+              },
+              {
+                "-Question": "4.\tThe ________ key is pressed to move the cursor to the next line."
+              },
+              {
+                "-Question": "5.\tThe ________ key erases anything typed to the right of the cursor."
+              }
+            ]
+          },
+		  {
+            "-QType": "FillUp",
+            "-Heading": "Write ‘T’ for true and ‘F’ for false:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tThere are two sets of alphabet keys on the keyboard.\t______"
+              },
+              {
+                "-Question": "2.\tThe space bar is used to give blanks spaces.\t\t\t______"
+              },
+              {
+                "-Question": "3.\tWe use the Enter keys to type numbers.\t\t\t\t______"
+              },
+              {
+                "-Question": "4.\tWhatever we type can be seen on monitor.\t\t\t______"
+              },
+              {
+                "-Question": "5.\tWe use the Alphabet keys to type letters.\t\t\t______"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Tick the correct option.",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tTo type your name, you will press the :\r\n\t(a) alphabet keys      (b) number keys     (c) delete key\r\n"
+              },
+              {
+                "-Question": "2.\tWhich key moves the cursor to the next line?\n\t(a) Delete      (b) Space bar    (c) Enter\n"
+              },
+              {
+                "-Question": "3.\tThe _____ key erases anything typed to the left of the cursor.\n\t(a) Backspace      (b) Enter   (c) Delete\n"
+              },
+              {
+                "-Question": "4.\t _____ keys are used to move the cursor in different directions.\n\t(a) Alphabet      (b) Arrow   (c) Number\n"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Answer the following questions.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWhat do we use to write on a computer?"
+              },
+              {
+                "-Question": "2.\tName the blinking line on the monitor that shows where we can start typing."
+              },
+              {
+                "-Question": "3.\tWhich key do we use to move the cursor to the next line?"
+              },
+              {
+                "-Question": "4.\tName the longest key on the keyboard."
+              },
+              {
+                "-Question": "5.\tWhich key is used to erase anything typed to the right of the cursor?"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Picture Based Question",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-OptionImage": "MST1/41.JPG"
+              }
+            ]
+          }
+        ]
+      },
+	  {
+        "-ChapterName": "Know the Mouse",
+        "-Heading": "Answer the following.",
+        "Cluster": [
+          {
+            "-QType": "FillUp",
+            "-Heading": "Fill in the blanks:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tA _______ is a special seat for a computer mouse."
+              },
+              {
+                "-Question": "2.\t _______ is generally used to select an object."
+              },
+              {
+                "-Question": "3.\t ________ is generally used to open a window."
+              },
+              {
+                "-Question": "4.\tThe arrow on the monitor is called the ________ ."
+              },
+              {
+                "-Question": "5.\tReleasing the mouse button after drag is called ________ ."
+              }
+            ]
+          },
+		  {
+            "-QType": "FillUp",
+            "-Heading": "Write ‘T’ for true and ‘F’ for false:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tA mouse helps us to draw pictures.\t\t\t\t\t\t\t______"
+              },
+              {
+                "-Question": "2.\tA computer mouse has a scroll wheel in the middle.\t\t\t______"
+              },
+              {
+                "-Question": "3.\tTo select an item, press the left mouse button twice quickly.\t______"
+              },
+              {
+                "-Question": "4.\tThe pointer move as we move the mouse.\t\t\t\t\t______"
+              },
+              {
+                "-Question": "5.\tA right-click shows a list of things you can do.\t\t\t\t______"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Tick the correct option.",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tA mouse is used to:\r\n\t(a) point and select items      (b) draw pictures     (c) both (a) and (b)\r\n"
+              },
+              {
+                "-Question": "2.\tA computer mouse has _____ buttons.\n\t(a) no      (b) one    (c) two\n"
+              },
+              {
+                "-Question": "3.\tThe small arrow on the monitor is called :\n\t(a) tail      (b) wire   (c) pointer\n"
+              },
+              {
+                "-Question": "4.\tWhich of the following is used to select an item?\n\t(a) Left-click      (b) Double-click   (c) Drag-and-drop\n"
+              },
+              {
+                "-Question": "5.\tIt shows a list of things one can do.\n\t(a) Left-click      (b) Double-click   (c) Right-click\n"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Answer the following questions.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWhere is the wire of the mouse attached?"
+              },
+              {
+                "-Question": "2.\tName the buttons on a mouse."
+              },
+              {
+                "-Question": "3.\tWhat is a mouse used for?"
+              },
+              {
+                "-Question": "4.\tWhat is a double-click?"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Picture Based Question",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-OptionImage": "MST1/51.JPG"
+              },
+			  {
+                "-OptionImage": "MST1/52.JPG"
+              },
+			  {
+                "-OptionImage": "MST1/53.JPG"
+              }
+            ]
+          }
+        ]
+      },
+	  {
+        "-ChapterName": "Using Computers The Right Way",
+        "-Heading": "Answer the following.",
+        "Cluster": [
+          {
+            "-QType": "FillUp",
+            "-Heading": "Fill in the blanks:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tThe _______ is a device that maintains a continuous supply of electricity."
+              },
+              {
+                "-Question": "2.\t ________ is the process of starting a computer."
+              },
+              {
+                "-Question": "3.\tClick the ________ button to shut down a computer."
+              },
+              {
+                "-Question": "4.\tWe should never use a ________ cloth to clean a computer."
+              },
+              {
+                "-Question": "5.\tDo not pull any wires connected to the ________ ."
+              }
+            ]
+          },
+		  {
+            "-QType": "FillUp",
+            "-Heading": "Write ‘T’ for true and ‘F’ for false:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tWe should not eat while working on a computer.\t\t______"
+              },
+              {
+                "-Question": "2.\tStarting a computer is a different process.\t\t\t______"
+              },
+              {
+                "-Question": "3.\tUPS stands for Uniform Power Supply.\t\t\t\t______"
+              },
+              {
+                "-Question": "4.\tIt is important to properly shut your computer down.\t______"
+              },
+              {
+                "-Question": "5.\tWe should use a dry cloth to clean a computer.\t\t______"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Tick the correct option.",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\t _____ maintains a continuous supply of electricity.\r\n\t(a) CPU      (b) UPS     (c) monitor\r\n"
+              },
+              {
+                "-OptionImage": "MST1/63.JPG"
+              },
+              {
+                "-Question": "3.\tStart button can be seen at the _____ of screen.\n\t(a) top      (b) bottom   (c) left\n"
+              },
+              {
+                "-Question": "4.\tUse _____ cloth to remove the dust from computer.\n\t(a) wet      (b) dry   (c) sticky\n"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Answer the following questions.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWhat is booting?"
+              },
+              {
+                "-Question": "2.\tWhat is the full form of UPS?"
+              },
+              {
+                "-Question": "3.\tWhere is Start button located?"
+              },
+              {
+                "-Question": "4.\tWrite any two don’ts to be followed in computer lab."
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Picture Based Question",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-OptionImage": "MST1/61.JPG"
+              }
+            ]
+          }
+        ]
+      },
+	  {
+        "-ChapterName": "Fun with Paint",
+        "-Heading": "Answer the following.",
+        "Cluster": [
+          {
+            "-QType": "FillUp",
+            "-Heading": "Fill in the blanks:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tThe _______ program is used to draw and colour on a computer."
+              },
+              {
+                "-Question": "2.\tTo draw a perfect square, press the ________ key."
+              },
+              {
+                "-Question": "3.\tWe select a tool by ________ on that tool."
+              },
+              {
+                "-Question": "4.\tWe bring the mouse pointer in the ________ before starting to draw."
+              },
+              {
+                "-Question": "5.\tThe ________ displays the program name."
+              }
+            ]
+          },
+		  {
+            "-QType": "FillUp",
+            "-Heading": "Write ‘T’ for true and ‘F’ for false:",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tThe Paint program is used to do sums.\t\t\t\t______"
+              },
+              {
+                "-Question": "2.\tYou can draw a rectangle using the oval shape.\t\t______"
+              },
+              {
+                "-Question": "3.\tFill with color tool is used to fill the colours.\t\t\t______"
+              },
+              {
+                "-Question": "4.\tThe shapes group on the ribbon has many shapes.\t______"
+              },
+              {
+                "-Question": "5.\tTo start Paint, click on All Apps first.\t\t\t\t______"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Tick the correct option.",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-Question": "1.\tWhere do you make drawings in Paint?\r\n\t(a) Colors group      (b) Ribbon     (c) Drawing Area\r\n"
+              },
+              {
+                "-Question": "2.\tRibbon has _____ tabs.\n\t(a) one      (b) two    (c) three\n"
+              },
+              {
+                "-Question": "3.\tTo draw a circle, we use the:\n\t(a) Line tool      (b) Pencil tool   (c) Oval tool\n"
+              },
+              {
+                "-Question": "4.\t _____ key is used to draw a perfect square.\n\t(a) Ctrl      (b) Shift   (c) Enter\n"
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Answer the following questions.",
+            "-defaultMark": "5",
+            "item": [
+              {
+                "-Question": "1.\tWhich part of the paint window do we use to draw pictures?"
+              },
+              {
+                "-Question": "2.\tWhich tool do we use to fill in colours?"
+              },
+              {
+                "-Question": "3.\tWrite the steps to start paint."
+              },
+              {
+                "-Question": "4.\tName the main parts of paint window."
+              }
+            ]
+          },
+          {
+            "-QType": "FillUp",
+            "-Heading": "Picture Based Question",
+            "-defaultMark": "1",
+            "item": [
+              {
+                "-OptionImage": "MST1/71.JPG"
+              },
+			  {
+                "-OptionImage": "MST1/72.JPG"
+              }
+            ]
+          }
+        ]
       }
     ]
   }
